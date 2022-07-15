84207fa1095ea5a7a8f781201ac12d2f0c418c29
Akshat <kaushal.m@samcomtechnobrains.com>
2022-07-15 17:34:40 +0530

    First Commit

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import classes from "../layout/Layout.module.css";
+import MainNavigation from "./MainNavigation.js";
+
+const Layout = (props) => {
+  return (
+    <Fragment>
+      <MainNavigation />
+      <main className={classes.main}> {props.children} </main>
+    </Fragment>
+  );
+};
+
+export default Layout;
